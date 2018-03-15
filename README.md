c58cb45acd009a6fabfbbefb6475bb9dff6b01fa
linbin <289873007@qq.com>
2018-03-15 20:56:48 +0800

    添加注释

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # node-upload-image
+
+## 前端
+设置upload的action为 url:port/upload(localhost:3000/upload), 接收filename返回值
+
+## 后端
+文件默认保存在`./uploads`下,读取也在这里读取.
+
+
+```
+git clone git@github.com:Linbubin/node-upload-image.git
+npm install
+npm start
+```
--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 var fs = require('fs');
 var express = require('express');
+var multer  = require('multer'); // 用来保存文件
 
+// 启express
 var app = express();
-var multer  = require('multer')
 
+// 配置multer
 var storage = multer.diskStorage({
+  // 保存位置
   destination: function(req, file, cb) {
-      cb(null, './node-image/uploads');
+      cb(null, './uploads');
   },
+  // 保存文件名
   filename: function(req, file, cb) {
       cb(null, `${Date.now()}-${file.originalname}`)
   }
 })
 var upload = multer({ storage: storage });
 
+// 增加跨域许可
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
@@ -21,19 +26,29 @@ app.all('*', function(req, res, next) {
   return next();
 });
 
+// 默认访问，可删除
 app.get('/', function (req,res){
   res.send('over');
 })
 
+// 读取img文件，并展示 url: localhost/photo?filename=111.jpg
 app.get('/photo', function (req,res){
+  // 传入url后面的参数
   const filename = req.query.filename;
   
-  fs.readFile(`./node-image/uploads/${filename}`,'binary',function(err, file) {
+  // 读取uploads下的文件
+  fs.readFile(`./uploads/${filename}`,'binary',function(err, file) {
+    // 如果读取失败(测试时，基本原因为文件不存在.真实环境中，应该不会读取失败)
     if(err){
-      fs.readFile(`./node-image/uploads/no_people.jpg`,'binary',function(err, file1) {
+      // 读取 no_people.jpg这张图
+      fs.readFile(`./uploads/no_people.jpg`,'binary',function(err, file1) {
+        // 这张图事先放好，保证读取成功
         if(err) return res.send(err);
+        // 设置 输出的head
         res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        // 将图片输出
         res.write(file1,'binary');
+        // end
         res.end();
       })
     }else{
@@ -45,10 +60,12 @@ app.get('/photo', function (req,res){
   })
 })
 
+// 用 multer将文件保存到相应位置，并将文件名返回
 app.post('/upload', upload.any(),function (req,res){
   res.send({filename: req.files[0].filename});
 })
 
+// 启动服务： 默认端口号为3000
 app.listen(3000, function(){
         console.log('ok')
 })
